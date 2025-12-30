e122cb7fbe8072fe6869dfd0274fe3ce7bf47200
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-12-30 09:03:38 -0600

    Create function to get the weather API data for a user input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-const x = "test";
+import "./style.css";
 
-function test() {
-  console.log("hi");
+const API_KEY = "5JKT896CV9G7SN2NK66PCYCW5";
+const form = document.querySelector("form");
+let location = "Tokyo";
+let weatherAPIdata = null;
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const userInput = document.querySelector("input").value.trim();
+    location = userInput;
+    console.log(location);
+    form.reset();
+    getAPIData();
+});
+
+async function getAPIData()
+{
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}/next6days?key=${API_KEY}`;
+
+    try {
+        let response = await fetch(url);
+        if(!response.ok)
+        {
+            throw new Error("HTTP Request Wrong");
+        }
+
+        weatherAPIdata = await response.json();
+        console.log(weatherAPIdata);
+
+    } catch (error) {
+        console.error("There was an error!: ",error);
+    }
 }
 
-function huh() {
-  console.log("huh");
-}
+//weatherAPIData.currentConditions
+//weatherAPIData.days ( [0] is today [1] is tmr etc)
+//conditions, temp, description, icon
